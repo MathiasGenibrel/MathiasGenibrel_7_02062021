4613c8f0df4e69a8b2c51218f8e3d65ab178fc55
mithivi <mathias.geni@gmail.com>
2021-06-18 19:44:52 +0200

    feat (React) Add react to project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxSign from './components/Sign';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BoxSign/>
+    </div>
+  );
+}
+export default App;
